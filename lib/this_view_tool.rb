8f531cbf200f5a7ea983a147a06d531cd055e5c9
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 17:40:43 +0000

    Dev tool initial commit

---
+++ lib/this_view_tool.rb
@@ -0,0 +1,6 @@
+require "this_view_tool/version"
+require "this_view_tool/renderer"
+
+module ThisViewTool
+  # Your code goes here...
+end
---
+++ lib/this_view_tool/version.rb
@@ -0,0 +1,3 @@
+module ThisViewTool
+  VERSION = "0.1.0"
+end
